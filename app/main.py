2a3c6251ed1e9a6424e16e97c674f9db4c3f2256
Zuzah <murtaza.hasni@gmail.com>
2024-09-29 12:54:15 -0400

    Added a requirements.txt. Also performed Ruff check on all project file to ensure correctly linted

--- app/main.py
+++ app/main.py
@@ -1,22 +1,25 @@
-from fastapi import FastAPI, File, UploadFile
+import pathlib
+
 from config.settings import settings
-from typing import Annotated
-import os
+from fastapi import FastAPI, UploadFile
 
 app = FastAPI()
 
 @app.get("/")
-def root():
+def root() -> str:
+    """Return the root message of the application."""
     return "root of application"
 
 @app.post("/upload_document/")
-async def create_upload_file(file: UploadFile):
+async def create_upload_file(file: UploadFile) -> dict[str,str]:
+    """Handle the upload of a document and save it to the specified folder."""
     folder = settings.upload_folder_path
-    
-    os.makedirs(folder, exist_ok=True)  # Create the folder if it doesn't exist
 
-    file_location = os.path.join(folder, file.filename)
-    with open(file_location, "wb+") as file_object:
+    p = pathlib.Path(folder)
+    p.mkdir(parents=True, exist_ok=True) # Create the folder if it doesn't exist
+
+    file_loc = folder+ "/" + file.filename
+    with pathlib.Path.open(file_loc, "wb+") as file_object:
         file_object.write(await file.read())  # Write the file to the directory
 
-    return {"info": "File saved", "filename": file.filename}
+    return {"info": "File saved", "filename": file.filename, "path": file_loc}
--- config/settings.py
+++ config/settings.py
@@ -1,11 +1,18 @@
-import os
 from pydantic_settings import BaseSettings
 
+
 class Settings(BaseSettings):
+    """Configuration settings for the FastAPI application.
+
+    The settings are automatically
+    loaded from the specified `.env` file, and can be overridden by
+    corresponding environment variables.
+    """
+
     app_name: str = "Rapid RAG"
     upload_folder_path: str # correlates to the same name found in the .env
 
     class Config:
-        env_file = '.env'
+        env_file = ".env"
 
-settings = Settings() 
+settings = Settings()
